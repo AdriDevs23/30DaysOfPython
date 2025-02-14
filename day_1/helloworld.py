fc0ce6f92f3d4cae151786ccb918c6eb4d3d57c3
“AdriDevs23” <adridevs23@gmail.com>
2025-02-14 17:04:38 +0100

    Lanzamiento del day_1 y el archivo readme

---
+++ day_1/helloworld.py
@@ -0,0 +1,63 @@
+# Exercise 2: We are going to use print to know the following information of Python:
+
+
+# (The operands are 3 and 4, so we go to declare both.)
+
+a = 3
+b = 4
+
+# Operations 
+
+# addition(+)
+print(a + b)
+
+# subtraction(-)
+print(a - b)
+
+# multiplication(*)
+print(a * b)
+
+# modulus(%)
+print(a % b)
+
+# division(/)
+print(a / b)
+
+# exponential(**)
+print(a ** b)
+
+# floor division operator(//)
+print(a // b)
+
+
+# Write strings on the python interactive shell. The strings are the following:
+
+# Your name
+print("Adri")
+
+# Your family name
+print("Carretero")
+
+# Your country
+print("Spain")
+
+# I am enjoying 30 days of python
+print("I am enjoying 30 days of python")
+
+# Check the data types of the following data:
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4- 4j))
+print(type(['Asabeneh', 'Python', 'Finland']))
+print(type ("Your name"))
+print(type ("Your family name"))
+print(type ("Your country"))
+
+
+# Exercise Level 3: Write an example for different Python data
+
+
+dictionary = {"Name": "Adri", "Age": 29, "Favourite football team": "Agrupación Deportiva Rayo Vallecano"}
+
+print(dictionary)
---
+++ readme.md
@@ -0,0 +1,3 @@
+
+
+This is a challenge from Asabeneh's GitHub (https://github.com/Asabeneh/30-Days-Of-Python.git) 30 Days Of Python 
